dd3b2fed2f456e1cd9daa14b7435fec4ee1d8bcf
KrisHite <alexgusevmail@gmail.com>
2023-05-29 23:28:07 +0500

    First commit

---
+++ src/main/java/ru/workassistantbot/gusev/GusevApplication.java
@@ -0,0 +1,13 @@
+package ru.workassistantbot.gusev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GusevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GusevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/workassistantbot/gusev/GusevApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.workassistantbot.gusev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GusevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
